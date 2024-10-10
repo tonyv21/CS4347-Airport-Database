30da20a5cee7b22b25c84eca2398c3b29de4bf5a
tonyv21 <111463221+tonyv21@users.noreply.github.com>
2024-10-09 20:02:00 -0500

    Create Tables Update

--- wip-database.sql
+++ wip-database.sql
@@ -1,69 +1,65 @@
-CREATE TABLE
-    IF NOT EXISTS airlines (
-        airline_id TEXT PRIMARY KEY,
-        name TEXT,
-        num_employess INTEGER,
-        num_planes INTEGER
-    );
+CREATE TABLE IF NOT EXISTS airplanes (
+    plane_id CHAR(10),
+    plane_type VARCHAR(50),
+    max_distance REAL CHECK (max_distance > 0),
+    capacity INTEGER CHECK (capacity > 0),
+    dry_weight REAL CHECK (dry_weight > 0),
+    airline_id CHAR(6),
+    FOREIGN KEY (airline_id) REFERENCES airlines (airline_id),
+    PRIMARY KEY (plane_id, plane_type)
+);
 
-CREATE TABLE
-    IF NOT EXISTS airports (
-        airport_id TEXT PRIMARY KEY,
-        name TEXT,
-        state TEXT,
-        city TEXT,
-        latitude REAL,
-        longitude REAL
-    );
+CREATE TABLE IF NOT EXISTS airlines (
+    airline_id CHAR(6) PRIMARY KEY,
+    name VARCHAR(100),
+    num_employees INTEGER CHECK (num_employees >= 0),
+    num_planes INTEGER CHECK (num_planes >= 0)
+);
 
-CREATE TABLE
-    IF NOT EXISTS airplanes (
-        plane_id TEXT,
-        plane_type TEXT,
-        max_distance REAL,
-        capacity REAL,
-        dry_weight REAL,
-        airline_id TEXT,
-        FOREIGN KEY (airline_id) REFERENCES airlines (airline_id),
-        PRIMARY KEY (plane_id, plane_type)
-    );
 
-CREATE TABLE
-    IF NOT EXISTS flights (
-        flight_id TEXT,
-        start_time TIMESTAMP,
-        end_time TIMESTAMP,
-        airplane_id TEXT,
-        start_id TEXT,
-        end_id TEXT,
-        FOREIGN KEY (airplane_id) REFERENCES airplanes (plane_id),
-        FOREIGN KEY (start_id) REFERENCES airports (airport_id),
-        FOREIGN KEY (end_id) REFERENCES airports (airport_id),
-        PRIMARY KEY (flight_id, start_id, end_id)
-    );
+CREATE TABLE IF NOT EXISTS airports (
+    airport_id CHAR(3) PRIMARY KEY,
+    name VARCHAR(100),
+    state CHAR(2),
+    city VARCHAR(100),
+    latitude DECIMAL(9, 6),
+    longitude DECIMAL(9, 6)
+);
 
-CREATE TABLE
-    IF NOT EXISTS passengers (
-        passenger_id TEXT PRIMARY KEY,
-        num_bags INTEGER,
-        fname TEXT,
-        lname TEXT,
-        midInit CHAR(1),
-        flight_id TEXT,
-        FOREIGN KEY (flight_id) REFERENCES flights (flight_id)
-    );
+CREATE TABLE IF NOT EXISTS flights (
+    flight_id CHAR(8),
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,
+    airplane_id CHAR(10),
+    start_id CHAR(3),
+    end_id CHAR(3),
+    FOREIGN KEY (airplane_id) REFERENCES airplanes (plane_id),
+    FOREIGN KEY (start_id) REFERENCES airports (airport_id),
+    FOREIGN KEY (end_id) REFERENCES airports (airport_id),
+    PRIMARY KEY (flight_id, start_id, end_id)
+);
 
-CREATE TABLE
-    IF NOT EXISTS employees (
-        employee_id TEXT,
-        airline_id INTEGER,
-        salary REAL,
-        type TEXT,
-        fname TEXT,
-        lname TEXT,
-        midInit CHAR(1),
-        license_id TEXT,
-        plane_id TEXT,
-        FOREIGN KEY (plane_id) REFERENCES airplanes (airplane_id),
-        PRIMARY KEY (employee_id, airline_id)
-    );
+CREATE TABLE IF NOT EXISTS passengers (
+    passenger_id CHAR(10) PRIMARY KEY,
+    num_bags INTEGER CHECK (num_bags >= 0),
+    fname VARCHAR(50),
+    lname VARCHAR(50),
+    midInit CHAR(1),
+    flight_id CHAR(8),
+    FOREIGN KEY (flight_id) REFERENCES flights (flight_id)
+);
+
+CREATE TABLE IF NOT EXISTS employees (
+    employee_id CHAR(10),
+    airline_id CHAR(6),
+    salary REAL CHECK (salary > 0),
+    type VARCHAR(50),
+    fname VARCHAR(50),
+    lname VARCHAR(50),
+    midInit CHAR(1),
+    license_id CHAR(10),
+    plane_id CHAR(10),
+    FOREIGN KEY (plane_id) REFERENCES airplanes (plane_id),
+    FOREIGN KEY (airline_id) REFERENCES airlines (airline_id),
+    PRIMARY KEY (employee_id, airline_id)
+);
