6ffd0919b3476c8afd6e4cbd149aa292fd8284a5
Brice Duke <74372754+briceduke@users.noreply.github.com>
2024-10-09 19:19:47 -0500

    Create airports-insert.sql

---
+++ airports-insert.sql
@@ -0,0 +1,38 @@
+INSERT INTO airports (airport_id, name, state, city, latitude, longitude)
+VALUES ('KATL', 'Hartsfield Jackson Atlanta International Airport', 'GA', 'Atlanta', 33.6367, -84.428101);
+
+INSERT INTO airports (airport_id, name, state, city, latitude, longitude)
+VALUES ('KAUS', 'Austin Bergstrom International Airport', 'TX', 'Austin', 30.197535, -97.662015);
+
+INSERT INTO airports (airport_id, name, state, city, latitude, longitude)
+VALUES ('KDEN', 'Denver International Airport', 'CO', 'Denver', 39.86169815, -104.6729965);
+
+INSERT INTO airports (airport_id, name, state, city, latitude, longitude)
+VALUES ('KDFW', 'Dallas Fort Worth International Airport', 'TX', 'Dallas-Fort Worth', 32.896801, -97.038002);
+
+INSERT INTO airports (airport_id, name, state, city, latitude, longitude)
+VALUES ('KIAH', 'George Bush Intercontinental Houston Airport', 'TX', 'Houston', 29.9843998, -95.34140015);
+
+INSERT INTO airports (airport_id, name, state, city, latitude, longitude)
+VALUES ('KJFK', 'John F Kennedy International Airport', 'NY', 'New York', 40.639447, -73.779317);
+
+INSERT INTO airports (airport_id, name, state, city, latitude, longitude)
+VALUES ('KLAX', 'Los Angeles International Airport', 'CA', 'Los Angeles', 33.942501, -118.407997);
+
+INSERT INTO airports (airport_id, name, state, city, latitude, longitude)
+VALUES ('KMCO', 'Orlando International Airport', 'FL', 'Orlando', 28.42939949, -81.30899811);
+
+INSERT INTO airports (airport_id, name, state, city, latitude, longitude)
+VALUES ('KMIA', 'Miami International Airport', 'FL', 'Miami', 25.79319954, -80.29060364);
+
+INSERT INTO airports (airport_id, name, state, city, latitude, longitude)
+VALUES ('KORD', 'Chicago OHare International Airport', 'IL', 'Chicago', 41.9786, -87.9048);
+
+INSERT INTO airports (airport_id, name, state, city, latitude, longitude)
+VALUES ('KPHX', 'Phoenix Sky Harbor International Airport', 'AZ', 'Phoenix', 33.435302, -112.005905);
+
+INSERT INTO airports (airport_id, name, state, city, latitude, longitude)
+VALUES ('KSEA', 'Seattle-Tacoma International Airport', 'WA', 'Seattle', 47.447943, -122.310276);
+
+INSERT INTO airports (airport_id, name, state, city, latitude, longitude)
+VALUES ('KTPA', 'Tampa International Airport', 'FL', 'Tampa', 27.9755, -82.533203);
