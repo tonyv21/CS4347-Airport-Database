97af182e59ae96695cc7e5d2a3e11cc3eb40af39
Brice Duke <74372754+briceduke@users.noreply.github.com>
2024-10-09 19:12:03 -0500

    Update wip-database.sql

--- wip-database.sql
+++ wip-database.sql
@@ -2,8 +2,8 @@ CREATE TABLE
     IF NOT EXISTS airlines (
         airline_id TEXT PRIMARY KEY,
         name TEXT,
-        num_employess INT,
-        num_planes INT,
+        num_employess INTEGER,
+        num_planes INTEGER
     );
 
 CREATE TABLE
@@ -18,30 +18,52 @@ CREATE TABLE
 
 CREATE TABLE
     IF NOT EXISTS airplanes (
-        plane_id TEXT PRIMARY KEY,
-        plane_type TEXT PRIMARY KEY,
+        plane_id TEXT,
+        plane_type TEXT,
         max_distance REAL,
         capacity REAL,
         dry_weight REAL,
+        airline_id TEXT,
         FOREIGN KEY (airline_id) REFERENCES airlines (airline_id),
+        PRIMARY KEY (plane_id, plane_type)
     );
 
 CREATE TABLE
     IF NOT EXISTS flights (
-        flight_id TEXT PRIMARY KEY,
+        flight_id TEXT,
         start_time TIMESTAMP,
         end_time TIMESTAMP,
+        airplane_id TEXT,
+        start_id TEXT,
+        end_id TEXT,
         FOREIGN KEY (airplane_id) REFERENCES airplanes (plane_id),
-        FOREIGN KEY (start_id) REFERENCES airports (airport_id) PRIMARY KEY,
-        FOREIGN KEY (end_id) REFERENCES airports (airport_id) PRIMARY KEY,
+        FOREIGN KEY (start_id) REFERENCES airports (airport_id),
+        FOREIGN KEY (end_id) REFERENCES airports (airport_id),
+        PRIMARY KEY (flight_id, start_id, end_id)
     );
 
 CREATE TABLE
     IF NOT EXISTS passengers (
         passenger_id TEXT PRIMARY KEY,
-        num_bags INT,
-        name STRUCT < fname TEXT,
+        num_bags INTEGER,
+        fname TEXT,
         lname TEXT,
-        midInit TEXT >,
-        FOREIGN KEY (flight_id) REFERENCES flights (flight_id),
+        midInit CHAR(1),
+        flight_id TEXT,
+        FOREIGN KEY (flight_id) REFERENCES flights (flight_id)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS employees (
+        employee_id TEXT,
+        airline_id INTEGER,
+        salary REAL,
+        type TEXT,
+        fname TEXT,
+        lname TEXT,
+        midInit CHAR(1),
+        license_id TEXT,
+        plane_id TEXT,
+        FOREIGN KEY (plane_id) REFERENCES airplanes (airplane_id),
+        PRIMARY KEY (employee_id, airline_id)
     );
