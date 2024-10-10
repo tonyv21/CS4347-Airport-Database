6c6e13ab74daa1b27e9e59bf90cc056785a7e312
Brice Duke <74372754+briceduke@users.noreply.github.com>
2024-10-09 20:21:43 -0500

    Create airplanes-insert.sql

---
+++ airplanes-insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('A220-1', 'Airbus A220', 6300, 135, 27215, 'DL');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('A320-1', 'Airbus A320', 6150, 150, 42600, 'AA');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('A330-1', 'Airbus A330', 13450, 275, 119000, 'UA');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('A330neo-1', 'Airbus A330neo', 13334, 275, 119000, 'AA');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('A350-1', 'Airbus A350 XWB', 15000, 325, 134000, 'DL');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('An148-1', 'Antonov An-148/An-158', 3500, 75, 24000, 'AIO');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('B737-1', 'Boeing 737', 5000, 160, 41000, 'WN');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('B767-1', 'Boeing 767', 11000, 210, 80000, 'UA');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('B777-1', 'Boeing 777', 15000, 350, 150000, 'AA');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('B787-1', 'Boeing 787 Dreamliner', 14140, 280, 120000, 'UA');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('ARJ21-1', 'Comac ARJ21 Xiangfeng', 2200, 85, 24000, 'NK');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('C919-1', 'Comac C919', 5555, 160, 42000, 'NK');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('E2-1', 'Embraer E-Jet E2 family', 5300, 110, 30000, 'B6');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('EJet-1', 'Embraer E-Jet family', 4450, 100, 28000, 'B6');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('Il96-1', 'Ilyushin Il-96', 11000, 270, 120000, 'AIO');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('SSJ100-1', 'Sukhoi Superjet SSJ100', 3000, 85, 24250, 'NK');
+INSERT INTO airplanes (plane_id, plane_type, max_distance, capacity, dry_weight, airline_id) VALUES ('Tu204-1', 'Tupolev Tu-204/Tu-214', 4300, 210, 58000, 'AIO');
