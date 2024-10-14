57d27b15190ca0274994f0bd411b02c857bb9dc3
RyanDux03 <113806051+RyanDux03@users.noreply.github.com>
2024-10-14 18:54:26 -0500

    Add files via upload

---
+++ flights_insert.sql
@@ -0,0 +1,44 @@
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('AIO3289', '2024-10-14 08:00:00', '2024-10-14 11:30:00', 'An148-1', 'KJFK', 'KLAX');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('AA5910', '2024-10-14 09:15:00', '2024-10-14 13:45:00', 'A330neo-1', 'KORD', 'KDFW');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('UA1738', '2024-10-14 10:30:00', '2024-10-14 14:00:00', 'B787-1', 'KMIA', 'KORD');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('DL4621', '2024-10-14 11:00:00', '2024-10-14 14:30:00', 'A220-1', 'KATL', 'KMCO');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('WN5843', '2024-10-14 12:00:00', '2024-10-14 15:30:00', 'B737-1', 'KLAX', 'KPHX');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('CO2197', '2024-10-14 12:30:00', '2024-10-14 16:00:00', 'B767-1', 'KIAH', 'KDEN');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('NW4305', '2024-10-14 13:15:00', '2024-10-14 16:45:00', 'Il96-1', 'KSEA', 'KORD');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('US7482', '2024-10-14 14:00:00', '2024-10-14 17:30:00', 'B767-1', 'KMIA', 'KDFW');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('B61304', '2024-10-14 15:00:00', '2024-10-14 18:30:00', 'E2-1', 'KJFK', 'KSEA');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('ENY6972', '2024-10-14 16:15:00', '2024-10-14 19:45:00', 'EJet-1', 'KMCO', 'KATL');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('MQ8519', '2024-10-14 17:00:00', '2024-10-14 20:30:00', 'A330neo-1', 'KAUS', 'KDFW');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('OO3874', '2024-10-14 17:45:00', '2024-10-14 21:15:00', 'B767-1', 'KDEN', 'KSEA');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('PI4402', '2024-10-14 18:30:00', '2024-10-14 22:00:00', 'Tu204-1', 'KJFK', 'KMIA');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('XE6901', '2024-10-14 19:15:00', '2024-10-14 22:45:00', 'SSJ100-1', 'KSEA', 'KTPA');
+
+INSERT INTO flights (flight_id, start_time, end_time, airplane_id, start_id, end_id)
+VALUES ('NK2145', '2024-10-14 20:00:00', '2024-10-14 23:30:00', 'ARJ21-1', 'KORD', 'KATL');
